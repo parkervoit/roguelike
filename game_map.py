cb6dcc3ef54aeb29b6a040e055cded344ccd14a4
Parker Voit <pvoit@skipcart.com>
2022-01-06 13:55:56 -0600

    added field of view functionality

--- game_map.py
+++ game_map.py
@@ -7,12 +7,23 @@ class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value = tile_types.wall, order = 'F')
+        self.visible = np.full((width, height), fill_value = False, order = "F")
+        self.explored = np.full((width, height), fill_value = False, order = "F")
 
     def in_bounds(self, x:int, y: int) -> bool:
         '''Return True if x and y are inside the map bounds'''
         return 0 <= x < self.width and 0 <= y < self.height
     
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles['dark']
+        '''
+        Renders the map
         
-    
+        if tile == visible, draw with light
+        if tile == explored, draw with dark
+        if tile != explored & tile != visible, draw with shroud
+        '''        
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist = [self.tiles['light'], self.tiles['dark']],
+            default = tile_types.shroud
+        )
--- tile_types.py
+++ tile_types.py
@@ -17,6 +17,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool), # True if can be walked on
         ("transparent", np.bool), #true if tile doesn't obstruct FOV
         ("dark", graphic_dt), #graphics fro when tile is not in FOV
+        ("light", graphic_dt), #for when the tile is in FOV
     ]
 )
 
@@ -24,15 +25,23 @@ def new_tile(
     *, #enforce use of keywords so parameter order doesnt matter
     walkable: int,
     transparent: int, 
-    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+    dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
 ) -> np.ndarray:
     '''helper function for defining individual tile types'''
-    return np.array((walkable, transparent, dark), dtype = tile_dt)
-
+    return np.array((walkable, transparent, dark, light), dtype = tile_dt)
+# SHROUD represents fog of war
+shroud = np.array((ord(" "), (255,255,255), (0, 0, 0)), dtype = graphic_dt)
 floor = new_tile(
-    walkable = True, transparent = True, dark = (ord(" "), (255,255,255), (50, 50, 150)),
+    walkable = True, 
+    transparent = True, 
+    dark = (ord(" "), (255,255,255), (50, 50, 150)),
+    light = (ord(" "), (255,255,255), (200, 180, 50)),
 )
 
 wall = new_tile(
-    walkable = False, transparent = False, dark=(ord(" "), (255,255,255), (0, 0, 100)),
+    walkable = False, 
+    transparent = False, 
+    dark=(ord(" "), (255,255,255), (0, 0, 100)),
+    light=(ord(" "), (255,255,255), (130,110,50))
 )
